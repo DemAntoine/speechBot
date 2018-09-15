f9709867269aa220eb33dddac9d6e7a80bd0e1a4
Pycharm <demydenko.anton@gmail.com>
2018-09-15 15:33:55 +0300

    beta version with 4 languages

--- polly2.py
+++ polly2.py
@@ -35,21 +35,16 @@ def speech(bot, update):
     if "AudioStream" in response:
         global output
         with closing(response["AudioStream"]) as stream:
-            #            output = tracks_dir + '/' + str(track_count) + " track-boto.mp3"
-
             output = os.path.join(tracks_dir, str(track_count) + " track-boto.mp3")
             try:
                 # Open a file for writing the output as a binary stream
                 with open(output, "wb") as file:
                     file.write(stream.read())
             except IOError as error:
-                # Could not write to file, exit gracefully
                 print(error)
                 sys.exit(-1)
 
-    #    full_name = os.path.join(tracks_dir, str(track_count) + " track-boto.mp3")
     track = open(output, 'rb')
-
     bot.sendAudio(chat_id=update.message.chat_id, audio=track)
 
 
@@ -61,32 +56,8 @@ def start_command(bot, update):
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     bot.sendMessage(chat_id=update.message.chat_id, reply_markup=reply_markup, parse_mode=ParseMode.HTML,
-                    text='<b>Бот конвертує текст в аудіо. Зараз мова Російська, можеш вибрати іншу</b>')
-
-
-# def set_language(bot, update):
-#     global chosen_language
-#     chosen_language = update.callback_query.data
-#     if chosen_language == 'Brian':
-#         bot.editMessageText(
-#             text='Ти обрав <b>англійську</b>. Пиши боту текст, і він пришле аудіо англійською.',
-#             chat_id=update.callback_query.message.chat_id, message_id=update.callback_query.message.message_id,
-#             parse_mode=ParseMode.HTML)
-#     elif chosen_language == 'Hans':
-#         bot.editMessageText(
-#             text='Ти обрав <b>німецьку</b>. Пиши боту текст, і він пришле аудіо німецькою.',
-#             chat_id=update.callback_query.message.chat_id, message_id=update.callback_query.message.message_id,
-#             parse_mode=ParseMode.HTML)
-#     elif chosen_language == 'Maxim':
-#         bot.editMessageText(
-#             text='Ти обрав <b>російську</b>. Пиши боту текст, і він пришле аудіо російською.',
-#             chat_id=update.callback_query.message.chat_id, message_id=update.callback_query.message.message_id,
-#             parse_mode=ParseMode.HTML)
-#     elif chosen_language == 'Giorgio':
-#         bot.editMessageText(
-#             text='Ти обрав <b>італійську</b>. Пиши боту текст, і він пришле аудіо італійською.',
-#             chat_id=update.callback_query.message.chat_id, message_id=update.callback_query.message.message_id,
-#             parse_mode=ParseMode.HTML)
+                    text='<b>Бот конвертує текст в аудіо. Зараз обрано {} мову, можеш вибрати іншу</b>'
+                    .format(language_keys[chosen_language]))
 
 
 def set_language(bot, update):
